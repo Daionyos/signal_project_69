db7d54f1e08a26d3473c600d7ac12e08f6be24cf
tj3b <tjebbedasbach@gmail.com>
2024-06-09 14:58:38 +0200

    final combined check alert

--- src/main/java/com/alerts/CombinedAlert.java
+++ src/main/java/com/alerts/CombinedAlert.java
@@ -12,6 +12,8 @@ public class CombinedAlert {
     public Alert check(List <PatientRecord> records){
         List <PatientRecord> bloodPressure = sorting(records,"SystolicPressure");
         List <PatientRecord> saturation = sorting(records, "Saturation");
+        Alert checking = combinedCheck(bloodPressure, saturation);
+        if(checking != null){return checking;}else{return null;}
         
     }
     public List <PatientRecord> sorting(List <PatientRecord> records, String parameter){
@@ -24,8 +26,28 @@ public class CombinedAlert {
         return list;
 
     }
-    public combinedCheck(List <PatientRecord> sys, List <PatientRecord> sat){
-        
+    public Alert combinedCheck(List <PatientRecord> sys, List <PatientRecord> sat){
+        List <PatientRecord> sysAlerts = new ArrayList<>();
+        List <PatientRecord> satAlerts = new ArrayList<>();
+
+        for(int i = 0; i < sys.size(); i++ ){
+            if(sys.get(i).getMeasurementValue() < 90.0){
+                sysAlerts.add(sys.get(i));
+            }
+        }
+        for(int i = 0; i < sat.size(); i++ ){
+            if(sat.get(i).getMeasurementValue() < 90.0){
+                satAlerts.add(sat.get(i));
+            }
+        }
+        for(int i = 0; i < sysAlerts.size(); i++){
+            for(int j = 0; j < satAlerts.size(); j++){
+                if(Math.abs(sysAlerts.get(i).getTimestamp() - satAlerts.get(j).getTimestamp()) >= 120000){
+                    return new Alert(Integer.toString(sysAlerts.get(i).getPatientId()), "Combined Alert: Hypotensive Hypoxemia Alert", sysAlerts.get(i).getTimestamp());
+                }
+            }
+        }
+        return null;
     }
 
     
