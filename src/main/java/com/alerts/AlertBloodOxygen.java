ba426bcb9170358f2e4c2f157324a1f622d8599a
Daionyos <tuncsabah.f@gmail.com>
2024-06-09 23:41:49 +0200

    commenting

--- src/main/java/com/alerts/AlertBloodOxygen.java
+++ src/main/java/com/alerts/AlertBloodOxygen.java
@@ -1,9 +1,9 @@
 package com.alerts;
 
-
+//This class represents an alert related to blood oxygen levels by extending the base Alert class.
 public class AlertBloodOxygen extends Alert {
+    //Constructor to initialize a blood oxygen alert with patient ID, condition, timestamp, and priority.
     public AlertBloodOxygen(String patientId, String condition, long timestamp, int priority){
-        super( patientId,  condition,  timestamp);
+        super(patientId, condition, timestamp);
     }
-    
 }
